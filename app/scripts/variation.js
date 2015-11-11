e93f7f8d631451bc84bde7d57ef96f73407c65d9
ArtturiSipila <artturi.sipila@etuma.com>
2015-11-11 10:51:19 +0200

    Added some styling

--- app/scripts/variation.js
+++ app/scripts/variation.js
@@ -1,32 +1,33 @@
 "use strict"
 var i,
-    sigmaInstance,
-    o,
-    g = {
-      nodes: [],
-      edges: []
-    },
-    L = 10,
-    step = 0,
-    topic = 'my topic',
-    kws = [
-      {keyword: 'dolor', phrases: ['Lorem ipsum','sit amet']},
-      {keyword: 'sed', phrases: ['consectetur adipiscing elit,', 'do eiusmod','tempor incididunt']},
-      {keyword: 'minim',phrases: ['Ut enim ad','veniam, quis nostrud exercitation']},
-      {keyword: 'reprehenderit', phrases: ['Duis aute irure dolor in', 'in voluptate velit esse']},
-      {keyword: 'fugiat', phrases: ['cillum dolore eu', 'nulla pariatur']},
-      {keyword: 'natus', phrases: ['Sed ut perspiciatis unde omnis iste', 'error']},
-      {keyword: 'voluptatem', phrases: ['Nemo enim', 'ipsam', 'quia voluptas', 'sit aspernatur',  'aut odit']},
-      {keyword: 'exercitationem', phrases: ['quis nostrum', 'ullam corporis']},
-      {keyword: 'commodi', phrases: ['nisi ut aliquid ex ea', 'consequatur?']},
-      {keyword: 'quam', phrases: ['velit esse', 'nihil molestiae consequatur']}
-      ],
-   kw_size = 0.9,
-   nodeHighlightColor = '#B85959';
+  sigmaInstance,
+  o,
+  g = {
+    nodes: [],
+    edges: []
+  },
+  L = 10,
+  step = 0,
+  topic = 'my topic',
+  kws = [
+    { keyword: 'dolor', phrases: ['Lorem ipsum', 'sit amet'] },
+    { keyword: 'sed', phrases: ['consectetur adipiscing elit,', 'do eiusmod', 'tempor incididunt'] },
+    { keyword: 'minim', phrases: ['Ut enim ad', 'veniam, quis nostrud exercitation'] },
+    { keyword: 'reprehenderit', phrases: ['Duis aute irure dolor in', 'in voluptate velit esse'] },
+    { keyword: 'fugiat', phrases: ['cillum dolore eu', 'nulla pariatur'] },
+    { keyword: 'natus', phrases: ['Sed ut perspiciatis unde omnis iste', 'error'] },
+    { keyword: 'voluptatem', phrases: ['Nemo enim', 'ipsam', 'quia voluptas', 'sit aspernatur', 'aut odit'] },
+    { keyword: 'exercitationem', phrases: ['quis nostrum', 'ullam corporis'] },
+    { keyword: 'commodi', phrases: ['nisi ut aliquid ex ea', 'consequatur?'] },
+    { keyword: 'quam', phrases: ['velit esse', 'nihil molestiae consequatur'] }
+  ],
+  kw_size = 0.9,
+  nodeHighlightColor = '#B85959',
+  nodeColorByPosition = false,
+  phraseNodeXOffset = 0.75,
+  phraseNodeYOffset = 1;
       
-
 // Generate a random graph:
-
 g.nodes.push({
   id: 'topicNode',
   label: topic,
@@ -67,11 +68,11 @@ for (i = 0; i < kws.length; i++) {
     selected: false,
     type: 'goo'
   };
-  
-  ['x', 'y', 'size', 'color'].forEach(function(val) {
+
+  ['x', 'y', 'size', 'color'].forEach(function (val) {
     o[val] = o['grid_' + val];
   });
-  
+
   console.log(o);
   g.nodes.push(o);
 }
@@ -88,102 +89,102 @@ for (i = 0; i < kws.length; i++)
   });
 
 
- /**
-   * CUSTOM RENDERERS:
-   * *****************
-   */
-  sigma.canvas.edges.goo = function(e, s, t, ctx, settings) {
-    var color = e.color,
-        p = settings('prefix') || '',
-        edgeColor = settings('edgeColor'),
-        defaultNodeColor = settings('defaultNodeColor'),
-        defaultEdgeColor = settings('defaultEdgeColor'),
-        v,
-        d,
-        p1 = 5 / 6,
-        p2 = 1 / 6;
+/**
+  * CUSTOM RENDERERS:
+  * *****************
+  */
+sigma.canvas.edges.goo = function (e, s, t, ctx, settings) {
+  var color = e.color,
+    p = settings('prefix') || '',
+    edgeColor = settings('edgeColor'),
+    defaultNodeColor = settings('defaultNodeColor'),
+    defaultEdgeColor = settings('defaultEdgeColor'),
+    v,
+    d,
+    p1 = 5 / 6,
+    p2 = 1 / 6;
 
-    if (!color)
-      switch (edgeColor) {
-        case 'source':
-          color = s.color || defaultNodeColor;
-          break;
-        case 'target':
-          color = t.color || defaultNodeColor;
-          break;
-        default:
-          color = defaultEdgeColor;
-          break;
-      }
+  if (!color)
+    switch (edgeColor) {
+      case 'source':
+        color = s.color || defaultNodeColor;
+        break;
+      case 'target':
+        color = t.color || defaultNodeColor;
+        break;
+      default:
+        color = defaultEdgeColor;
+        break;
+    }
 
-    d = Math.sqrt(Math.pow(t[p + 'x'] - s[p + 'x'], 2) + Math.pow(t[p + 'y'] - s[p + 'y'], 2));
-    v = {
-      x: (t[p + 'x'] - s[p + 'x']) / d,
-      y: (t[p + 'y'] - s[p + 'y']) / d
-    };
-
-    ctx.fillStyle = color;
-    ctx.beginPath();
-    ctx.moveTo(
-      s[p + 'x'] + v.y * s[p + 'size'],
-      s[p + 'y'] - v.x * s[p + 'size']
-    );
-    ctx.bezierCurveTo(
-      s[p + 'x'] * p1 + t[p + 'x'] * p2 + v.y * e[p + 'size'],
-      s[p + 'y'] * p1 + t[p + 'y'] * p2 - v.x * e[p + 'size'],
-      t[p + 'x'] * p1 + s[p + 'x'] * p2 + v.y * e[p + 'size'],
-      t[p + 'y'] * p1 + s[p + 'y'] * p2 - v.x * e[p + 'size'],
-      t[p + 'x'] + v.y * t[p + 'size'],
-      t[p + 'y'] - v.x * t[p + 'size']
-    );
-    ctx.lineTo(
-      t[p + 'x'] - v.y * t[p + 'size'],
-      t[p + 'y'] + v.x * t[p + 'size']
-    );
-    ctx.bezierCurveTo(
-      t[p + 'x'] * p1 + s[p + 'x'] * p2 - v.y * e[p + 'size'],
-      t[p + 'y'] * p1 + s[p + 'y'] * p2 + v.x * e[p + 'size'],
-      s[p + 'x'] * p1 + t[p + 'x'] * p2 - v.y * e[p + 'size'],
-      s[p + 'y'] * p1 + t[p + 'y'] * p2 + v.x * e[p + 'size'],
-      s[p + 'x'] - v.y * s[p + 'size'],
-      s[p + 'y'] + v.x * s[p + 'size']
-    );
-    ctx.closePath();
-    ctx.fill();
+  d = Math.sqrt(Math.pow(t[p + 'x'] - s[p + 'x'], 2) + Math.pow(t[p + 'y'] - s[p + 'y'], 2));
+  v = {
+    x: (t[p + 'x'] - s[p + 'x']) / d,
+    y: (t[p + 'y'] - s[p + 'y']) / d
   };
 
-  sigma.canvas.nodes.goo = function(node, ctx, settings) {
-    var prefix = settings('prefix') || '';
-
-    ctx.fillStyle = node.color || settings('defaultNodeColor');
-    ctx.beginPath();
-    ctx.arc(
-      node[prefix + 'x'],
-      node[prefix + 'y'],
-      node[prefix + 'size'],
-      0,
-      Math.PI * 2,
-      true
+  ctx.fillStyle = color;
+  ctx.beginPath();
+  ctx.moveTo(
+    s[p + 'x'] + v.y * s[p + 'size'],
+    s[p + 'y'] - v.x * s[p + 'size']
     );
-    ctx.closePath();
-    ctx.fill();
-
-    ctx.fillStyle = '#fff';
-    ctx.beginPath();
-    ctx.arc(
-      node[prefix + 'x'],
-      node[prefix + 'y'],
-      node[prefix + 'size'] * 0.5,
-      0,
-      Math.PI * 2,
-      true
+  ctx.bezierCurveTo(
+    s[p + 'x'] * p1 + t[p + 'x'] * p2 + v.y * e[p + 'size'],
+    s[p + 'y'] * p1 + t[p + 'y'] * p2 - v.x * e[p + 'size'],
+    t[p + 'x'] * p1 + s[p + 'x'] * p2 + v.y * e[p + 'size'],
+    t[p + 'y'] * p1 + s[p + 'y'] * p2 - v.x * e[p + 'size'],
+    t[p + 'x'] + v.y * t[p + 'size'],
+    t[p + 'y'] - v.x * t[p + 'size']
     );
-    ctx.closePath();
-    ctx.fill();
-  };
+  ctx.lineTo(
+    t[p + 'x'] - v.y * t[p + 'size'],
+    t[p + 'y'] + v.x * t[p + 'size']
+    );
+  ctx.bezierCurveTo(
+    t[p + 'x'] * p1 + s[p + 'x'] * p2 - v.y * e[p + 'size'],
+    t[p + 'y'] * p1 + s[p + 'y'] * p2 + v.x * e[p + 'size'],
+    s[p + 'x'] * p1 + t[p + 'x'] * p2 - v.y * e[p + 'size'],
+    s[p + 'y'] * p1 + t[p + 'y'] * p2 + v.x * e[p + 'size'],
+    s[p + 'x'] - v.y * s[p + 'size'],
+    s[p + 'y'] + v.x * s[p + 'size']
+    );
+  ctx.closePath();
+  ctx.fill();
+};
+
+sigma.canvas.nodes.goo = function (node, ctx, settings) {
+  var prefix = settings('prefix') || '';
+
+  ctx.fillStyle = node.color || settings('defaultNodeColor');
+  ctx.beginPath();
+  ctx.arc(
+    node[prefix + 'x'],
+    node[prefix + 'y'],
+    node[prefix + 'size'],
+    0,
+    Math.PI * 2,
+    true
+    );
+  ctx.closePath();
+  ctx.fill();
+
+  ctx.fillStyle = '#fff';
+  ctx.beginPath();
+  ctx.arc(
+    node[prefix + 'x'],
+    node[prefix + 'y'],
+    node[prefix + 'size'] * 0.5,
+    0,
+    Math.PI * 2,
+    true
+    );
+  ctx.closePath();
+  ctx.fill();
+};
 
 // "methods have to be added before instances are created to make them available""
-sigma.classes.graph.addMethod('getNodesCount', function() {
+sigma.classes.graph.addMethod('getNodesCount', function () {
   return this.nodesArray.length;
 });
 
@@ -209,17 +210,17 @@ sigmaInstance = new sigma({
     doubleClickEnabled: false,
     //minEdgeSize: 0.5,
     //maxEdgeSize: 4,
-    enableEdgeHovering: true,
+    //minNodeSize: 1,
+    maxNodeSize: 20,
+    enableEdgeHovering: false,
     edgeHoverColor: '#222',
     defaultEdgeHoverColor: '#222',
     edgeHoverSizeRatio: 5,
     edgeHoverExtremities: true,
-    
     animationsTime: 1000,
     sideMargin: 1,
     defaultLabelSize: 18,
     //font: "Lato"
-    
   }
 });
 
@@ -227,48 +228,43 @@ var prefix = 'circular_';
 var toggleAnimation = function (callback) {
 
   //calculate colors based on the new position
-  /*
-  var conId = sigmaInstance.renderers[0].conradId;
-  var nodesOnGraph = sigmaInstance.graph.nodes();
-  var mapX = sigmaInstance.graph.nodes().map(function (o) {
-    return o['renderer' + conId + ':x'];
-  })
-
-  var mapY = sigmaInstance.graph.nodes().map(function (o) {
-    return o['renderer' + conId + ':y'];
-  })
-
-  var xMax = Math.max.apply(Math, mapX)
-  var xMin = Math.min.apply(Math, mapX)
-  var xRange = xMax - xMin;
-  var yMax = Math.max.apply(Math, mapY)
-  var yMin = Math.min.apply(Math, mapY)
-  var yRange = yMax - yMin;
-
-
-  for (var i = 0, x, y, z, a, b, c; i < nodesOnGraph.length; i++) {
-    x = Math.pow((xMax - nodesOnGraph[i]['renderer' + conId + ':x']) / (xRange), 2) * 16;
-    y = Math.pow((yMax - nodesOnGraph[i]['renderer' + conId + ':y']) / (yRange), 2) * 16;
-    z = x + y / 2;
-
-    a = Math.min(Math.round(x), 15).toString(16);
-    b = Math.min(Math.round(y), 15).toString(16);
-    c = Math.round((16 - x + z) / 2).toString(16); //out of various spatial conditions this worked out the best. You can play around with this. 
-
-
-    a = a + a;
-    b = b + b;
-    c = c + c;
-
-
-    //console.log(a+b+a)
-
-
-    nodesOnGraph[i].color = "#" + a + b + c
-
+  if (nodeColorByPosition) {
+    var conId = sigmaInstance.renderers[0].conradId;
+    var nodesOnGraph = sigmaInstance.graph.nodes();
+    var mapX = sigmaInstance.graph.nodes().map(function (o) {
+      return o['renderer' + conId + ':x'];
+    })
+  
+    var mapY = sigmaInstance.graph.nodes().map(function (o) {
+      return o['renderer' + conId + ':y'];
+    })
+  
+    var xMax = Math.max.apply(Math, mapX)
+    var xMin = Math.min.apply(Math, mapX)
+    var xRange = xMax - xMin;
+    var yMax = Math.max.apply(Math, mapY)
+    var yMin = Math.min.apply(Math, mapY)
+    var yRange = yMax - yMin;
+  
+  
+    for (var i = 0, x, y, z, a, b, c; i < nodesOnGraph.length; i++) {
+      x = Math.pow((xMax - nodesOnGraph[i]['renderer' + conId + ':x']) / (xRange), 2) * 16;
+      y = Math.pow((yMax - nodesOnGraph[i]['renderer' + conId + ':y']) / (yRange), 2) * 16;
+      z = x + y / 2;
+  
+      a = Math.min(Math.round(x), 15).toString(16);
+      b = Math.min(Math.round(y), 15).toString(16);
+      c = Math.round((16 - x + z) / 2).toString(16); //out of various spatial conditions this worked out the best. You can play around with this. 
+  
+      a = a + a;
+      b = b + b;
+      c = c + c;
+  
+      //console.log(a+b+a)
+      nodesOnGraph[i].color = "#" + a + b + c
+    }
+    sigmaInstance.refresh();
   }
-  sigmaInstance.refresh();
-  */
 
 
   sigma.plugins.animate(
@@ -281,7 +277,7 @@ var toggleAnimation = function (callback) {
     },
     {
       onComplete: function () {
-        console.log("animation complete");
+        console.log("layout animation complete");
         if (callback) callback();
       }
     }
@@ -294,7 +290,8 @@ var animatePhrases = function (callback) {
     sigmaInstance,
     {
       x: prefix + 'x',
-      y: prefix + 'y',      
+      y: prefix + 'y',
+      size: prefix + 'size'
     },
     {
       onComplete: function () {
@@ -305,31 +302,13 @@ var animatePhrases = function (callback) {
     );
 };
 
-
-
 toggleAnimation();
 /**
    * EVENTS BINDING:
    * ***************
    */
-  //console.log("dom: ",dom)
   
-  // Bind the events:
- /*
-s.bind('overNode outNode clickNode doubleClickNode rightClickNode', function(e) {
-  console.log(e.type, e.data.node.label, e.data.captor);
-});
-s.bind('overEdge outEdge clickEdge doubleClickEdge rightClickEdge', function(e) {
-  console.log(e.type, e.data.edge, e.data.captor);
-});
-s.bind('clickStage', function(e) {
-  console.log(e.type, e.data.captor);
-});
-s.bind('doubleClickStage rightClickStage', function(e) {
-  console.log(e.type, e.data.captor);
-});
-*/
-var tempNodes = [];
+var phraseNodes = [];
 sigmaInstance.bind('clickNode', function (e) {
   //console.log(e);
   
@@ -338,11 +317,9 @@ sigmaInstance.bind('clickNode', function (e) {
   sigmaInstance.graph.nodes().forEach(function (n) {
           
     //drop temp nodes if they exist
-    tempNodes.forEach(function (tempNode) {
+    phraseNodes.forEach(function (tempNode) {
       if (n.id === tempNode.id) sigmaInstance.graph.dropNode(tempNode.id);
     })
-
-    tempNodes = [];  //clear the tempNodes arrays
             
     if (n.id === e.data.node.id) {
       n.color = nodeHighlightColor;       //use the highlight color
@@ -353,76 +330,54 @@ sigmaInstance.bind('clickNode', function (e) {
 
   });
 
-
-
-
-
   if (e.data.node.id === 'topicNode') {
-    prefix = 'circular_';
-
-
-
+    prefix = 'circular_';   //clicking a topic node will make a transition to circular layout
     toggleAnimation(null);
   }
-  else {
+  else {                    //otherwise we have clicked a keyword node and we use the grid layout
     prefix = 'grid_';
-     
-    //e.data.node.color = '#AA0000';
-    console.log(e.data.node);
-
-
 
     if (!e.data.node.selected) {  //new selection
        
       var callback = null;
 
-
-
       sigmaInstance.graph.nodes().forEach(function (n) {
-        // if (toKeep[n.id])
-        //   n.color = n.originalColor;
-        // else
-        //  n.color = '#eee';
-        //console.log(n);
-         
-        console.log("node is not selected")
+        
         if (n.id === e.data.node.id) {
 
+          //define the animation callback, phrase nodes will be added after the animation is done
           callback = function () {
-
-            console.log("animation callback!");
-                
-            //temporary nodes
-            //var i=0;
-            console.log("n: ", n);
-            //n.phrases.forEach(function(val) {
+            //before we push any new nodes to the array we'll clean it up
+            phraseNodes = [];
+            
+            //phrase nodes
             for (var i = 0; i < n.phrases.length; i++) {
-              var temp = {
-                id: 'tempNode_' + i,
+              var phraseNode = {
+                id: 'phraseNode_' + i,
                 size: 1,
-                grid_size: 1,
+                grid_size: 0.75,
                 x: n.grid_x,
                 y: n.grid_y,
-                grid_x: n.grid_x + 0.75,
-                grid_y: n.grid_y + + 0.5 + i * 0.5,
+                grid_x: n.grid_x + phraseNodeXOffset,
+                grid_y: n.grid_y + phraseNodeYOffset + i * phraseNodeYOffset,
                 label: n.phrases[i],
                 type: 'goo'
               };
 
-              tempNodes.push(temp);
-              sigmaInstance.graph.addNode(temp);
+              phraseNodes.push(phraseNode);
+              sigmaInstance.graph.addNode(phraseNode);
 
               sigmaInstance.graph.addEdge({
-                id: 'tempEdge_' + i,
-                source: temp.id,
+                id: 'phraseEdge_' + i,
+                source: phraseNode.id,
                 target: n.id,
+                size: 1,
+                grid_size: 0.1,
                 type: 'goo'
               });
 
               if (i > 5) break;
             };
-
-
 
             sigmaInstance.refresh();
             animatePhrases(null);  //no animatePhrases callback
@@ -433,26 +388,14 @@ sigmaInstance.bind('clickNode', function (e) {
           n.selected = true;
         }
         else {
-            
-            
-          //restore properties
-          //n.grid_size = kw_size;
-          //n.grid_x = n.original_grid_x;
-            
-           
-          if (n.id !== 'tempNodeBefore' || n.id !== 'tempNodeAfter') {
-            n.grid_x = -n.original_grid_x;
-          }
-          //n.grid_size *= 0.5;
+          n.grid_x = -n.original_grid_x;
           n.selected = false; //unselect other nodes
         }
-
 
       });
     }
     else {  //already selected
       console.log("node was already selected");
-
     }
         
     // Since the data has been modified, we need to
@@ -462,6 +405,6 @@ sigmaInstance.bind('clickNode', function (e) {
       sigmaInstance.refresh();
 
     toggleAnimation(callback);
-    callback = null;     
+    callback = null;
   }
 });
